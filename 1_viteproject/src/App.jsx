5abc245c369fedf1a005c5d92a21ca0a1a46f16e
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-21 06:50:20 +0530

    Learn React working flow

---
+++ 1_viteproject/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import Mota from "./index";
+function App() {
+  return (
+    <div>
+      <Mota/>
+      <h1>Sayan Dan</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 1_viteproject/src/index.jsx
@@ -0,0 +1,5 @@
+function Mota() {
+  return <h1>hello up kaysa ho</h1>;
+}
+
+export default Mota;
