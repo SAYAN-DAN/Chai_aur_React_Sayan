ae441cbdd358eca5c6ac6781110da9b3403e517c
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-25 07:10:38 +0530

    Interview_Question

---
+++ 6_Interview/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(15);
+
+  const addValue = function () {
+    // if (count < 30) {
+    //   setCount(count+1)
+    // }
+
+    // count = count +1
+    setCount((prevCounter) =>(prevCounter + 1));
+    setCount((prevCounter) =>(prevCounter + 1));
+    setCount((prevCounter) =>(prevCounter + 1));
+    setCount((prevCounter) =>(prevCounter + 1));
+  };
+
+  const removeValue = function () {
+    //  if (count > 0) {
+    //   setCount(count - 1)
+    //  }
+  };
+
+  return (
+    <>
+      <h1 className="mb-6 text-7xl bg-gray-950 p-6">Sayan Dan</h1>
+      <button className="mb-5 bg-slate-950 " onClick={addValue}>
+        Add Value:{count}
+      </button>
+      <br />
+      <br />
+      <button className="mb-5 bg-slate-950 " onClick={removeValue}>
+        Remove Value:{count}
+      </button>
+    </>
+  );
+}
+
+export default App;
